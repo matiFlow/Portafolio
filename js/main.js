b84ca88f0d36566523133c4b3ca1499835d8de6e
felix kevin acarapi rivero <kevin.acarapi@itscordoba.edu.ar>
2023-06-19 19:37:19 -0300

    nueva actualizacion del proyecto

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Obtener referencias a los elementos necesarios
-const hamburguesa = document.querySelector('.hamburgesa');
-const menuNavegacion = document.querySelector('header nav ul');
-
-// Agregar evento de clic a la hamburguesa
-hamburguesa.addEventListener('click', function() {
-  menuNavegacion.classList.toggle('active');
-});
